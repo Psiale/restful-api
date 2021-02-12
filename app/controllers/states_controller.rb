1947b33997b6121d296bd2fb049e42746742255f
psialedev <psialedev@gmail.com>
2021-02-12 16:23:08 -0600

    created the state controller, and added a response module

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,4 +1,6 @@
 class StatesController < ApplicationController
+  before_action :set_state, only: [:show, :update, :destroy]
+
   def index
     @states = State.all
     json_response(@states)
@@ -7,6 +9,30 @@ class StatesController < ApplicationController
   def create
     @state = State.create(state_params)
     json_response(@state, :created)
+  end
+
+  def show
+    json_response(@state)    
+  end
+
+  def update
+    @state.update(state_params)
+    head :no_content
+  end
+
+  def destroy
+    @state.destroy
+    head :no_content
+  end
+
+  private
+
+  def state_params
+    params.permit(:name)
+  end
+
+  def set_state
+    @state = State.find(params[:id])
   end
 
 end
